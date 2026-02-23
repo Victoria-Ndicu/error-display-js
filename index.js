05121b2a9653177ed9212fc031f7b8c772c9157a
VvieyAB <ndicuvictoria@gmail.com>
2026-02-23 14:58:12 +0300

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+let errorMess = document.getElementById("error")
+function displayError(){
+    errorMess.textContent="Something went wrong, please try again"
+        
+}
+
+
+
